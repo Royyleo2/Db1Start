ab0d825c3b205df49db6b9e7f2643e68c73d787f
Royyleo2 <royyleo2@gmail.com>
2022-05-19 00:02:32 -0300

    Create variavel004.py

---
+++ variavel004.py
@@ -0,0 +1,6 @@
+nota1 = float(input('Primeira nota:'))
+nota2 = float(input('Segunda nota:'))
+nota3 = float(input('Terceira nota:'))
+nota4 = float(input('Quarta nota:'))
+media = (nota4 + nota3 + nota2 + nota1) / 4
+print ('A média entre as notas é de {}'.format (media))
