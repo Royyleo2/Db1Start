6a54769a49170bb1145a84d4e083235bafb44a5c
Royyleo2 <royyleo2@gmail.com>
2022-05-19 00:12:52 -0300

    Delete variavel006.py

--- variavel006.py
+++
@@ -1,4 +0,0 @@
-from math import pi
-raio = float(input('Qual o raio da circunferencia?'))
-area = pi*(raio ** 2)
-print ('A área da circunferência em que tenha o raio {} é de {:.2f}'.format (raio, area))
