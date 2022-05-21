ff7f6d0816cd7792756acbf25c9e4add0ed76f12
Royyleo2 <royyleo2@gmail.com>
2022-05-21 18:32:14 -0300

    Update repetições006.py

---
+++ Db1Python/Meus Projetos/repetições/repetições006.py
@@ -0,0 +1,19 @@
+print ('Gerador de PA')
+print ('*-' * 10)
+primeiro = int(input('Digite o primeiro termo:'))
+razão = int(input('Digite a razão da PA'))
+termo = primeiro
+cont = 1
+total = 0
+mais = 10
+while mais != 0:
+    total = total + mais
+    while cont <= total:
+        print ('{} ->'.format(termo), end=' ')
+        termo += razão
+        cont += 1
+        print ('PAUSA')
+        mais = int(input('Quantos termos você gostaria de ver a mais?'))
+print ('Você viu {} de termos'.format(total))
+print ("*-*" * 10)
+print ('FIM')
