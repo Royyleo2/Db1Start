3ffe540b1ad11e7dc71c2a0e214e762559254ca4
Royyleo2 <royyleo2@gmail.com>
2022-05-21 18:32:08 -0300

    Update repetições005.py

---
+++ Db1Python/Meus Projetos/repetições/repetições005.py
@@ -0,0 +1,11 @@
+print ('GERADOR DE PA')
+print ('-=-' * 10)
+primeiro = int(input('Primeiro termo:'))
+razão = int(input('Razão da PA:'))
+termo = primeiro
+cont = 1
+while cont <= 20:
+        print ('{} ->'.format (termo), end=' ')
+        termo = termo + razão
+        cont = cont + 1
+print ('FIM')
